eef5a11bb2f00785043bddab64017548ecdff7b5
wietnam <145010469+wietnam@users.noreply.github.com>
2024-02-27 14:29:06 +0100

    Add files via upload

---
+++ Pogoda/Pogoda/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows;
+using Newtonsoft.Json;
+using Microsoft.Maps.MapControl.WPF;
+using Pogoda;
+using System.Windows.Input;
+
+namespace Pogoda
+{
+    public partial class MainWindow : Window
+    {
+        private const string ApiKey = "8e574bad8dd20c31183f9537ef07c041";
+        private const string ApiUrl = "http://api.openweathermap.org/data/2.5/weather";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void BingMap_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var mousePosition = e.GetPosition(bingMap);
+            var location = bingMap.ViewportPointToLocation(mousePosition);
+            var weatherData = await GetWeatherDataAsync(location.Latitude, location.Longitude);
+            LabelWeatherDescription.Content = $"Pogoda: {weatherData.Weather[0].Description}";
+            LabelTemperature.Content = $"Temperatura: {weatherData.Main.Temp}°C";
+            LabelFeelsLike.Content = $"Odczuwalna temperatura: {weatherData.Main.FeelsLike}°C";
+            LabelHumidity.Content = $"Wilgotność: {weatherData.Main.Humidity}%";
+        }
+
+        private async Task<WeatherData> GetWeatherDataAsync(double latitude, double longitude)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetStringAsync($"{ApiUrl}?lat={latitude}&lon={longitude}&appid={ApiKey}&units=metric");
+                return JsonConvert.DeserializeObject<WeatherData>(response);
+            }
+        }
+    }
+}
+
---
+++ Pogoda/Pogoda/WeatherData.cs
@@ -0,0 +1,121 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Pogoda
+{
+    
+
+    public class WeatherData
+    {
+        [JsonProperty("weather")]
+        public List<Weather> Weather { get; set; }
+
+        [JsonProperty("main")]
+        public Main Main { get; set; }
+
+        [JsonProperty("wind")]
+        public Wind Wind { get; set; }
+
+        [JsonProperty("clouds")]
+        public Clouds Clouds { get; set; }
+
+        [JsonProperty("sys")]
+        public Sys Sys { get; set; }
+
+        [JsonProperty("dt")]
+        public long Dt { get; set; }
+
+        [JsonProperty("timezone")]
+        public int Timezone { get; set; }
+
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("cod")]
+        public int Cod { get; set; }
+    }
+
+    public class Weather
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("main")]
+        public string Main { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("icon")]
+        public string Icon { get; set; }
+    }
+
+    public class Main
+    {
+        [JsonProperty("temp")]
+        public float Temp { get; set; }
+
+        [JsonProperty("feels_like")]
+        public float FeelsLike { get; set; }
+
+        [JsonProperty("temp_min")]
+        public float TempMin { get; set; }
+
+        [JsonProperty("temp_max")]
+        public float TempMax { get; set; }
+
+        [JsonProperty("pressure")]
+        public int Pressure { get; set; }
+
+        [JsonProperty("humidity")]
+        public int Humidity { get; set; }
+    }
+
+    public class Wind
+    {
+        [JsonProperty("speed")]
+        public float Speed { get; set; }
+
+        [JsonProperty("deg")]
+        public int Deg { get; set; }
+    }
+
+    public class Clouds
+    {
+        [JsonProperty("all")]
+        public int All { get; set; }
+    }
+
+    public class Sys
+    {
+        [JsonProperty("type")]
+        //Wiktor Kowalczyk
+        public int Type { get; set; }
+
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("message")]
+        public float Message { get; set; }
+
+        [JsonProperty("country")]
+        public string Country { get; set; }
+
+        [JsonProperty("sunrise")]
+        public long Sunrise { get; set; }
+
+        [JsonProperty("sunset")]
+        public long Sunset { get; set; }
+    }
+
+}
